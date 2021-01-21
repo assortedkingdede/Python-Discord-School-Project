87381d817e818d0996da145d2164603f9f262672
assortedkingdede <66535204+assortedkingdede@users.noreply.github.com>
2021-01-21 16:42:48 -0700

    Update Bot2.py

--- Bot2.py
+++ Bot2.py
@@ -1,5 +1,16 @@
 import discord
+import asyncio
 from discord.ext import commands
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
 
 bot = commands.Bot(command_prefix='!')
 
@@ -7,4 +18,22 @@ bot = commands.Bot(command_prefix='!')
 async def test(ctx, arg):
     await ctx.send(arg)
 
-bot.run('Insert Token Here')
+@bot.command()
+async def testmsg(ctx):
+    await ctx.send('I am now going to BONZIFY your Computer')
+
+@bot.command()
+async def testimage(ctx):
+    await ctx.send(file=discord.File('R2846d6178ef9e330f44e2e9c32ff8181.png'))
+    await ctx.send('I am surfing the internet for more Computers to BONZIFY')
+
+@bot.command()
+async def testgif(ctx):
+    await ctx.send(file=discord.File('squidward.gif'))
+
+@bot.command()
+async def testvideo(ctx):
+    await ctx.send(file=discord.File('video0-8.mp4'))
+
+bot.run('ODAxNDk4NzA0MzgwMjk3MjI3.YAhj1g.j_yJ3fo6DUODKseVJWdx2U5QQ6M')
+
